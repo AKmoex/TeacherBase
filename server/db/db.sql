599ba6381c3998a9b896eae2d7d8397288ac2480
AKmoex <akmoex@gmail.com>
2022-05-24 13:22:30 +0800

    feat: update schema
    add Table Education, Family, Archive

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,32 +1,99 @@
-DROP SEQUENCE IF EXISTS dep_auto_inc;
-CREATE SEQUENCE dep_auto_inc
-    MINVALUE 1
-    MAXVALUE 9999999999999
-    START WITH 1
-    INCREMENT BY 1;
+-- 创建一个自增序列, 用于Department表的主键自增
+drop sequence if exists dep_auto_inc cascade;
+create sequence dep_auto_inc
+    minvalue 1
+    maxvalue 9999999999999
+    start with 1
+    increment by 1;
 
-DROP TABLE IF EXISTS Department CASCADE;
-CREATE TABLE Department(
-    id INTEGER PRIMARY KEY DEFAULT nextval('dep_auto_inc'),
-    name VARCHAR(32) UNIQUE,
-    establish_date DATE,
-    phone VARCHAR(32)
+drop table if exists Department cascade;
+create table Department(
+    id integer primary key default nextval('dep_auto_inc'),
+    name varchar(32) unique,
+    establish_date date,
+    phone varchar(32)
 );
 
-ALTER SEQUENCE dep_auto_inc OWNED BY Department.id;
+-- 将自增学列 dep_auto_inc 与Department关联, 实现主键自增
+alter sequence dep_auto_inc owned by department.id;
 
-DROP TABLE IF EXISTS Teacher CASCADE;
-CREATE TABLE Teacher(
-    id CHAR(8) PRIMARY KEY,
-    name VARCHAR(32),
-    sex TINYINT CHECK (sex IN (1,2)),
-    entry_date DATE,
-    term_date DATE,
-    phone VARCHAR(32),
-    department INTEGER,
-    password VARCHAR(100) NOT NULL DEFAULT '',
-    FOREIGN KEY (department) REFERENCES Department(id)
+
+drop table if exists Teacher cascade;
+create table Teacher(
+    id char(8) primary key,
+    name varchar(32),
+    sex tinyint check (sex in (0,1,2)),
+    entry_date date,
+    term_date date,
+    phone varchar(32),
+    department integer,
+    password varchar(100) not null default '',
+    foreign key (department) references department(id),
+    position varchar(256),
+    job varchar(256)
 );
-INSERT INTO Teacher VALUES('00000000','张三',1,'2000-01-30','2000-01-30','18755005131',NULL,'123456');
-INSERT INTO Teacher VALUES('00000001','李四',1,'2000-01-30','2000-01-30','18755005131',NULL,'123456');
-INSERT INTO Teacher VALUES('00000002','王五',1,'2000-01-30','2000-01-30','18755005131',NULL,'123456');
+
+
+-- 教育经历
+drop sequence if exists edu_auto_inc cascade;
+create sequence edu_auto_inc
+    minvalue 1
+    maxvalue 9999999999999
+    start with 1
+    increment by 1;
+drop table if exists Education cascade;
+create table Education(
+    id integer primary key default nextval('edu_auto_inc'),
+    teacher char(8),
+    start_date date,
+    end_date date,
+    school varchar(256),
+    Degree varchar(32) check (Degree in ('本科','硕士','博士','博士后')),
+    foreign key (teacher) references Teacher(id)
+);
+alter sequence edu_auto_inc owned by Education.id;
+
+
+-- 家庭关系
+drop sequence if exists fam_auto_inc cascade;
+create sequence fam_auto_inc
+    minvalue 1
+    maxvalue 9999999999999
+    start with 1
+    increment by 1;
+drop table if exists Family cascade;
+create table Family(
+    id integer primary key default nextval('fam_auto_inc'),
+    teacher char(8),
+    name varchar(32),
+    relation varchar(32),
+    phone varchar(32),
+    foreign key (teacher) references Teacher(id)
+);
+alter sequence fam_auto_inc owned by Family.id;
+
+
+-- 奖惩记录
+drop sequence if exists arc_auto_inc cascade;
+create sequence arc_auto_inc
+    minvalue 1
+    maxvalue 9999999999999
+    start with 1
+    increment by 1;
+drop table if exists Archive cascade;
+create table Archive(
+    id integer primary key default nextval('arc_auto_inc'),
+    teacher char(8),
+    title varchar(256),
+    detail text,
+    type tinyint check (type in (0,1)),
+    foreign key (teacher) references Teacher(id)
+);
+alter sequence arc_auto_inc owned by Archive.id;
+
+
+
+
+insert into teacher values('00000000','张三',1,'2000-01-30','2000-01-30','18755005131',null,'123456','副院长','玩');
+insert into teacher values('00000001','李四',1,'2000-01-30','2000-01-30','18755005131',null,'123456','老师','教学神');
+insert into teacher values('00000002','王五',1,'2000-01-30','2000-01-30','18755005131',null,'123456','副教授','学');
